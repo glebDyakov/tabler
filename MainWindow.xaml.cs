3218398d2f49a3a117832b986b69111acd3903b9
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-11-13 17:02:12 +0300

    add_evaluation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace tabler
 {
@@ -28,6 +29,7 @@ namespace tabler
         public TextBlock activeColumn;
         public int columnsPerRow = 21;
         public int rowsPerColumn = 20;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +38,7 @@ namespace tabler
             debugger = new SpeechSynthesizer();
             activeRow = startRow;
             activeColumn = startColumn;
+            formula.Focus();
         }
 
         private void HoverRowHandler(object sender, MouseEventArgs e)
@@ -60,13 +63,29 @@ namespace tabler
 
         private void SelectCellHandler(object sender, MouseButtonEventArgs e)
         {
+            if(activeCell.Text.Length >= 1 && activeCell.Text[0] == '=')
+            {
+                //debugger.Speak("это ячейка формула");
+                string expression = activeCell.Text.Substring(1);
+                int repeats = 0;
+                foreach (char expressionSymbol in expression)
+                {
+                    if (expressionSymbol == '=')
+                    {
+                        repeats++;
+                    }
+                }
+                if (repeats == 0)
+                {
+                    double result = CSharpScript.EvaluateAsync<double>(expression).Result;
+                    activeCell.Text = result.ToString();
+                }
+            }
             activeCell.Background = System.Windows.Media.Brushes.Transparent;
             activeCell = (TextBlock)sender;
             activeCell.Background = System.Windows.Media.Brushes.Green;
             int actualColumn = Grid.GetColumn(activeCell);
             int actualRow = Grid.GetRow(activeCell);
-            /*debugger.Speak("строчка " + actualRow);
-            debugger.Speak("колонка" + actualColumn);*/
             activeRow.Background = System.Windows.Media.Brushes.LightGray;
             activeColumn.Background = System.Windows.Media.Brushes.LightGray;
             activeColumn = ((TextBlock)paper.Children[actualColumn]);
@@ -76,9 +95,95 @@ namespace tabler
             formula.Text = activeCell.Text;
         }
 
-        private void InputFormulaHandler(object sender, TextCompositionEventArgs e)
+
+        private void InputFormulaHandler(object sender, TextChangedEventArgs e)
         {
             activeCell.Text = formula.Text;
+        }
+
+        private void SelectCells(object sender, MouseButtonEventArgs e)
+        {
+            //debugger.Speak("Выделаяю колонку или строчку");
+            activeCell = (TextBlock)sender;
+            int actualColumn = Grid.GetColumn(activeCell);
+            int actualRow = Grid.GetRow(activeCell);
+            activeColumn = ((TextBlock)paper.Children[actualColumn]);
+            // выделяем строчки
+            for (int rowIndex = 2; rowIndex < paper.ColumnDefinitions.Count; rowIndex++)
+            {
+                ((TextBlock)paper.Children[columnsPerRow * (actualRow + 1) * rowIndex]).Background = System.Windows.Media.Brushes.DarkGray;
+            }
+            // сбрасываем выделенные колонки
+            for (int rowIndex = 1; rowIndex < paper.ColumnDefinitions.Count; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < paper.RowDefinitions.Count; columnIndex++)
+                {
+                    if (columnIndex == 0)
+                    {
+                        // нужно раскрашивать шапку колокни
+                        ((TextBlock)paper.Children[rowIndex + 21 * columnIndex]).Background = System.Windows.Media.Brushes.LightGray;
+                    }
+                    else
+                    {
+                        ((TextBlock)paper.Children[rowIndex + 21 * columnIndex]).Background = System.Windows.Media.Brushes.White;
+                    }
+                }
+            }
+            // выделяем колонки
+            for (int columnIndex = 1; columnIndex < paper.RowDefinitions.Count; columnIndex++)
+            {
+                ((TextBlock)paper.Children[paper.Children.IndexOf(activeCell) + 21 * columnIndex]).Background = System.Windows.Media.Brushes.DarkGray;
+            }
+        }
+
+        private void SelectReverseCells(object sender, MouseButtonEventArgs e)
+        {
+            activeCell = (TextBlock)sender;
+            int actualColumn = Grid.GetColumn(activeCell);
+            int actualRow = Grid.GetRow(activeCell);
+            activeColumn = ((TextBlock)paper.Children[actualColumn]);
+            // выделяем строчки
+            for (int rowIndex = 2; rowIndex < paper.ColumnDefinitions.Count; rowIndex++)
+            {
+                ((TextBlock)paper.Children[columnsPerRow * (actualRow + 1) * rowIndex]).Background = System.Windows.Media.Brushes.DarkGray;
+            }
+            // сбрасываем выделенные колонки
+            for (int rowIndex = 1; rowIndex < paper.ColumnDefinitions.Count; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < paper.RowDefinitions.Count; columnIndex++)
+                {
+                    if (columnIndex == 0)
+                    {
+                        // нужно раскрашивать шапку колокни
+                        ((TextBlock)paper.Children[rowIndex + 21 * columnIndex]).Background = System.Windows.Media.Brushes.LightGray;
+                    }
+                    else
+                    {
+                        ((TextBlock)paper.Children[rowIndex + 21 * columnIndex]).Background = System.Windows.Media.Brushes.White;
+                    }
+                }
+            }
+            // выделяем колонки
+            for (int columnIndex = 1; columnIndex < paper.RowDefinitions.Count; columnIndex++)
+            {
+                ((TextBlock)paper.Children[paper.Children.IndexOf(activeCell) + 21 * columnIndex]).Background = System.Windows.Media.Brushes.DarkGray;
+            }
+        }
+
+        private void CancelBtnHandler(object sender, MouseButtonEventArgs e)
+        {
+            formula.Text = "";
+            Keyboard.ClearFocus();
+        }
+
+        private void ApplyBtnHandler(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void InsertFormulaBtnHandler(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
